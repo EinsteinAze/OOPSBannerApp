841dd65ed8c4238bf57b75d6711ee78c8ced5fa9
EinsteinAze <robostein2007@gmail.com>
2026-02-17 16:33:56 +0530

    UC3 Done

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,9 @@
 public class OOPSBannerApp {
   public static void main(String[] args) {
-    System.out.println("  ***   " + "  ***   " + " *****  " + "  **** ");
-    System.out.println(" **  ** " + " **  ** " + " **  ** " + " **     ");
-    System.out.println(" **  ** " + " **  ** " + " *****  " + " *****  ");
-    System.out.println(" **  ** " + " **  ** " + " **     " + "     ** ");
-    System.out.println("  ***   " + "  ***   " + " **     " + " ****git*  ");
+    System.out.println(String.join(" ", " ***  ", " ***  ", " ***** ", " ***** "));
+    System.out.println(String.join(" ", "**  **", "**  **", "**  ** ", "**     "));
+    System.out.println(String.join(" ", "**  **", "**  **", "*****  ", " ***** "));
+    System.out.println(String.join(" ", "**  **", "**  **", "**     ", "    ** "));
+    System.out.println(String.join(" ", " ***  ", " ***  ", "**     ", " ***** "));
   }
-
 }
